d87e2346b512bb9c49a0622577556e46a7b61308
Balthasar Hofer <lebalz@outlook.com>
2020-08-25 11:23:38 +0200

    make pip installation configurable

--- src/extension.ts
+++ src/extension.ts
@@ -52,6 +52,7 @@ const DEFAULT_USER_SETTINGS = {
   "editor.minimap.enabled": false,
   "workbench.settings.useSplitJSON": true,
   "python.languageServer": "Pylance",
+  "breadcrumbs.enabled": false,
 };
 
 function setConfig() {
@@ -71,7 +72,7 @@ function setConfig() {
 
 function configure(force: boolean = false) {
   const configuration = vscode.workspace.getConfiguration();
-  const skip = configuration.get("gbsl.ignore_configurations", false);
+  const skip = configuration.get("gbsl.ignoreConfigurations", false);
   if (skip && !force) {
     vscode.window.showInformationMessage(
       "GBSL Configuration is ignored. Edit your settings"
@@ -91,74 +92,93 @@ function extensionVersion(context: vscode.ExtensionContext): string {
   return packageFile?.version ?? "0.0.1";
 }
 
+function uninstallWrongPipVersions() {
+  return vscode.commands
+    .executeCommand("python2go.pipPackages")
+    .then((pkgs: any) => {
+      return pkgs as PipPackage[];
+    })
+    .then((packages) => {
+      const toUninstall = PIP_PACKAGES.filter((pkg) =>
+        packages.some(
+          (installed) =>
+            installed.package === pkg.package &&
+            pkg.version !== undefined &&
+            installed.version !== pkg.version
+        )
+      );
+      return new Promise((resolve) => {
+        if (toUninstall.length === 0) {
+          return resolve(true);
+        }
+        return resolve(
+          vscode.commands.executeCommand(
+            "python2go.pip",
+            `uninstall -y ${toUninstall.map((p) => p.package).join(" ")}`
+          )
+        );
+      });
+    })
+    .then(() => {
+      return true;
+    });
+}
+
+function installPipPackages() {
+  if (vscode.workspace.getConfiguration().get("gbsl.ignorePipInstall", false)) {
+    return new Promise((resolve) => resolve(false));
+  }
+  return uninstallWrongPipVersions()
+    .then(() => {
+      return vscode.commands.executeCommand("python2go.pipPackages");
+    })
+    .then((pkgs: any) => {
+      return pkgs as PipPackage[];
+    })
+    .then((packages) => {
+      const toInstall = PIP_PACKAGES.filter(
+        (pkg) =>
+          !packages.some((installed) => installed.package === pkg.package)
+      );
+      if (toInstall.length > 0) {
+        const target = process.platform === "win32" ? "--user" : "";
+        const toInstallPkgs = toInstall.map((pkg) =>
+          pkg.version ? `${pkg.package}==${pkg.version}` : pkg.package
+        );
+        return vscode.commands
+          .executeCommand(
+            "python2go.pip",
+            `install ${target} ${toInstallPkgs.join(" ")}`
+          )
+          .then(() => {
+            return vscode.commands.executeCommand(
+              "workbench.action.reloadWindow"
+            );
+          });
+      }
+      return new Promise((resolve) => resolve());
+    });
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   const configuration = vscode.workspace.getConfiguration();
-  vscode.commands
-    .executeCommand("python2go.isPythonInstalled")
-    .then((isInstalled) => {
-      if (isInstalled) {
-        vscode.commands
-          .executeCommand("python2go.pipPackages")
-          .then((pkgs: any) => {
-            const packages = pkgs as PipPackage[];
-            const toUninstall = PIP_PACKAGES.filter((pkg) =>
-              packages.some(
-                (installed) =>
-                  installed.package === pkg.package &&
-                  pkg.version !== undefined &&
-                  installed.version !== pkg.version
-              )
-            );
-            console.log(toUninstall);
-            return new Promise((resolve) => {
-              if (toUninstall.length === 0) {
-                return resolve(true);
-              }
-              return resolve(
-                vscode.commands.executeCommand(
-                  "python2go.pip",
-                  `uninstall -y ${toUninstall.map((p) => p.package).join(" ")}`
-                )
-              );
-            }).then(() => {
-              const toInstall = PIP_PACKAGES.filter(
-                (pkg) =>
-                  !packages.some(
-                    (installed) => installed.package === pkg.package
-                  )
-              );
-              if (toInstall.length > 0) {
-                const target = process.platform === "win32" ? "--user" : "";
-                const toInstallPkgs = toInstall.map((pkg) =>
-                  pkg.version ? `${pkg.package}==${pkg.version}` : pkg.package
-                );
-                return vscode.commands.executeCommand(
-                  "python2go.pip",
-                  `install ${target} ${toInstallPkgs.join(" ")}`
-                );
-              }
-              return new Promise((resolve) => resolve());
-            });
-          })
-          .then(() => {
-            const configVersion = (context.globalState.get("configVersion") ??
-              "0.0.0") as string;
-            if (configuration.get("gbsl.ignore_configurations", false)) {
-              return;
-            }
-            const pluginVersion = extensionVersion(context);
-            const updateConfig = pluginVersion > configVersion;
+  installPipPackages().then(() => {
+    const configVersion = (context.globalState.get("configVersion") ??
+      "0.0.0") as string;
+    if (configuration.get("gbsl.ignoreConfigurations", false)) {
+      return;
+    }
+    const pluginVersion = extensionVersion(context);
+    const updateConfig = pluginVersion > configVersion;
 
-            if (updateConfig) {
-              return configure(true).then(() => {
-                context.globalState.update("configVersion", pluginVersion);
-              });
-            }
-          });
-      }
-    });
+    if (updateConfig) {
+      return configure(true).then(() => {
+        context.globalState.update("configVersion", pluginVersion);
+      });
+    }
+  });
 
   let configureDisposer = vscode.commands.registerCommand(
     "gbsl.configure",
